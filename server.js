835989cedf0e8623ea3865d5c3d3e18b0cd18be3
dmarie <daynamarie@github.com>
2018-07-06 16:38:34 -0400

    almost there

--- server.js
+++ server.js
@@ -1,63 +1,26 @@
-// Dependecies
 const express = require("express");
-const mongoose = require("mongoose");
-const bluebird = require("bluebird");
 const bodyParser = require("body-parser");
-const path = require("path");
-
-// Set up a default port, configure mongoose, configure our middleware
-const PORT = process.env.PORT || 8080;
-mongoose.Promise = bluebird;
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+///Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Serve up static assets if in production (running on Heroku)
+///Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-} else {
-  app.use(express.static(__dirname + "/client/public"));
+  app.use(express.static("client/public"));
 }
+app.use(routes);
 
-// enable CORS, use:
-// https://enable-cors.org/server_expressjs.html
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next();
-});
+///Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/nytarticles"
+);
 
-// Routing
-var articlesController = require("./server/controllers/article-controller");
-var router = new express.Router();
-// Define any API routes first
-// Get saved articles
-router.get("/api/saved", articlesController.find);
-// Save articles
-router.post("/api/saved", articlesController.insert);
-// delete saved articles
-router.delete("/api/saved/:id", articlesController.delete);
-// Send every other request to the React app
-router.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.use(router);
-
-// Connect mongoose to our database
-const db = process.env.MONGODB_URI || "mongodb://localhost/nyt-react";
-mongoose.connect(db, function(error) {
-  // Log any errors connecting with mongoose
-  if (error) {
-    console.error(error);
-  }
-  // Or log a success message
-  else {
-    console.log("mongoose connection is successful");
-  }
-});
-
-// Start the server
-app.listen(PORT, function() {
-  console.log(`Hello World==> Server now on port ${PORT}!`);
-});
+///Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
